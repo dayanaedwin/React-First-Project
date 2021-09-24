4b457687c7478c5c5c5e10ad42ac3087f3c40838
dayanaedwin <47363640+dayanaedwin@users.noreply.github.com>
2021-09-24 11:07:27 +0530

    adding components

---
+++ src/components/Movie.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './Movie.module.css';
+
+const Movie = (props) => {
+	return (
+		<li className={classes.movie}>
+			<h2>
+				{props.title} ({props.releaseDate})
+			</h2>
+			<p>{props.openingText}</p>
+		</li>
+	);
+};
+
+export default Movie;
